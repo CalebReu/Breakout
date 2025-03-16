d6fe79e6b1d860fe9cf52b4b5c8eeb190dcfb32e
CalebReu <caleb.reurink@gmail.com>
2025-03-16 12:35:51 -0700

    added mouth

--- Assets/_Scripts/Logic/Face.cs
+++ Assets/_Scripts/Logic/Face.cs
@@ -5,6 +5,8 @@ public class Face : MonoBehaviour
 {
     [SerializeField] private Transform leftPupil;
     [SerializeField] private Transform rightPupil;
+    [SerializeField] private Transform mouth;
+    [SerializeField] private float frownDistance = 5f;
     private Transform leftEye;
     private Transform rightEye;
     private float maxPupilMovement;
@@ -20,6 +22,7 @@ public class Face : MonoBehaviour
 
     void Update()
     {
+        // Move the pupils to look at the ball
         Vector3 directionToBall = ball.position - leftEye.position;
         directionToBall = Vector3.ProjectOnPlane(Quaternion.Euler(-90, 0, 0) * directionToBall, Vector3.forward).normalized;
         leftPupil.localPosition = directionToBall * maxPupilMovement + new Vector3(0, 0, -0.05f);
@@ -27,5 +30,16 @@ public class Face : MonoBehaviour
         directionToBall = Vector3.ProjectOnPlane(Quaternion.Euler(-90, 0, 0) * directionToBall, Vector3.forward).normalized;
         Debug.Log("Direction to ball: " + directionToBall);
         rightPupil.localPosition = directionToBall * maxPupilMovement + new Vector3(0, 0, -0.05f);
+
+        // Move the mouth to frown when the ball is far away
+        float distanceToBall = Vector3.Distance(mouth.position, ball.position);
+        if (distanceToBall > frownDistance)
+        {
+            mouth.rotation = Quaternion.Euler(90, 0, 180);
+        }
+        else
+        {
+            mouth.rotation = Quaternion.Euler(90, 0, 0);
+        }
     }
 }
